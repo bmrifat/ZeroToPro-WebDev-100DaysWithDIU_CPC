dc9914cd3e07b022bee424cd83ee53c9e35ffdb4
bm rifat <bmrifat93@gmail.com>
2025-09-10 23:51:46 +0600

    1 st day in js

---
+++ 100days challenge/day 10-5/variable.js
@@ -0,0 +1,20 @@
+/**
+ * VARIABLE
+ * 5 things to declare a variable
+ */
+
+/**
+ * Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+ */
+
+var ihaveTaka = 1000;
+var buying = 700;
+var returnMoney = ihaveTaka - buying;
+console.log(returnMoney);
