6aeb9667fc003a87f921be59966f78773d75e5fc
bm rifat <bmrifat93@gmail.com>
2025-09-10 23:54:46 +0600

    property in js

---
+++ 100days challenge/day 10-5/property.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'Shundor Gadha',
+    age: 25,
+    profession: 'developer',
+    salary: 2500000,
+    married: true
+}
+// console.log(person) 
+// console.log(person.salary)
+// dot notation 
+const beyatta = person.married;
+// console.log(beyatta) 
+
+// bracket notation 
+console.log(person['age'])
