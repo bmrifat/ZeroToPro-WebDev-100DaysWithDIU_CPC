e8e5541777c30b3afbfeedef83772626b1104f11
bm rifat <bmrifat93@gmail.com>
2025-09-11 23:14:10 +0600

    explore JS

---
+++ 100days challenge/day 10-5/slice.js
@@ -0,0 +1,6 @@
+const address = 'andorkillabandortola';
+const part = address.slice(10, 16);
+console.log(part);
+
+const sentence = 'I am a good boy but lazy person who love to sleep more than work.';
+console.log(sentence.split(' '));
